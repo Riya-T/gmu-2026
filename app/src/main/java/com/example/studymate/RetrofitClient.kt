ca0ee7b9054436840801ce377e67f6aefc66861a
Reni Kaza <renikaza4757@gmail.com>
2026-02-15 09:16:49 -0500

    fixed chatbot on physical device

--- app/src/main/java/com/example/studymate/RetrofitClient.kt
+++ app/src/main/java/com/example/studymate/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     //private const val BASE_URL = "http://10.172.242.65:3000/"
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://192.168.100.45:3000/"
     // 10.0.2.2 = localhost for Android emulator
 
     val api: ChatApi by lazy {
